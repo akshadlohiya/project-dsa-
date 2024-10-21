c993a6b181ee661055364624cb5135dd4742c222
temp <temp@kali>
2024-10-21 09:32:05 -0400

    I have two problem how should i hide username and password of ftp server and how do i check is the server up

--- ftp-server-connection.c
+++ ftp-server-connection.c
@@ -1,4 +1,10 @@
 #include <stdio.h>
+#include <stdlib.h>
 #include "fvcs.h"
 
-
+void ftp() {
+	system("ftp 192.168.74.130");
+	system("temp");
+	system("123456");
+	system("cd ./storage");
+}
--- main.c
+++ main.c
@@ -1,3 +1,13 @@
 #include <stdio.h>
+#include <string.h>
 #include "fvcs.h"
 #include <dirent.h>
+
+int main(char *argv[], int argc) {
+	if(strcmp(argv[1], fvcs) == 0) 
+		ftp();
+	else {
+		printf("Wrong input\n");
+		return;
+	}
+}
