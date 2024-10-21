f5c7174d4692da9fad1b2bd26b6c6ae3dc213795
temp <temp@kali>
2024-10-21 10:06:56 -0400

    included else if option main to determine which function to run

--- main.c
+++ main.c
@@ -4,10 +4,23 @@
 #include <dirent.h>
 
 int main(char *argv[], int argc) {
-	if(strcmp(argv[1], fvcs) == 0) 
+	if(strcmp(argv[1], "fvcs") == 0) 
 		ftp();
 	else {
 		printf("Wrong input\n");
-		return;
+		return 1;
 	}
+	if(strcmp(argv[2], "commit")) {
+		commit();
+	}
+	else if(strcmp(argv[2], "add") == 0) {
+		add();
+	}
+	else if(strcmp(argv[2], "history") == 0) {
+		history();
+	}
+	else {
+		return 2;
+	}
+	return 0;
 }
