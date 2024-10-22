718774a96c8888812664a49084121913f7145a63
temp <temp@kali>
2024-10-22 08:38:50 -0400

    added a complex datastructure which set the ponters to ver in lists of lists

--- datastruct.c
+++ datastruct.c
@@ -1,3 +1,42 @@
 #include <stdio.h>
 #include "fvcs.h"
 #include <dirent.h>
+#include <stdlib.h>
+#include <errno.h>
+
+void init(p *st) {
+	node *r = *st;
+	r = NULL;
+}
+
+node *insert_node(p *st, char *dir) {
+	node *nn, *r = *st;
+	lists *l;
+	nn = (node *)malloc(sizeof(node));
+	l = (lists *)malloc(sizeof(lists));
+	if(nn) {
+		nn->dir = opendir(dir);
+		if(!dir) {
+			perror("opendir");
+			return;
+		}
+		nn->fl = l;
+		nn->bl = NULL;
+		nn->fl->forw = NULL;
+		nn->fl->back = NULL;
+		nn->fl->next = NULL;
+		nn->fl->prev = NULL;
+	}
+	if(!r) {
+		r = nn;
+	}
+	else {
+		while(r->l->forw)
+			r = r->l->forw;
+		r->fl->forw = nn;
+		nn->bl = r->fl
+		r->fl->back = r;
+		r = r->fl->forw;
+	}
+	return r;
+}
--- fvcs.h
+++ fvcs.h
@@ -1,1 +1,13 @@
 #include <stdio.h>
+#include <stdlib.h>
+#include <dirent.h>
+
+typedef struct node {
+	DIR *dir;
+	lists *fl, *bl;
+} node;
+typedef  node* p;
+typedef struct lists {
+	node *forw, *back;
+	lists *next, *prev;
+} lists;
